ea92892bea45a75b7ef56e831d001230ace72700
ShogunPanda <shogun_panda@me.com>
2013-08-11 16:57:50 -0700

    Fixed installation method.

--- lib/pincerna/version.rb
+++ lib/pincerna/version.rb
@@ -16,7 +16,7 @@ module Pincerna
     MINOR = 0
 
     # The patch version.
-    PATCH = 3
+    PATCH = 4
 
     # The current version of pincerna.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
